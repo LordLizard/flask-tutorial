3f50a0745ffe875822af7f89f91e7c08d51b8de1
Hary Moreno <haryjeje@gmail.com>
2024-06-09 21:16:40 -0400

    Initial base setup and database implementation.

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,1 +1,42 @@
 
+import sqlite3
+
+import click
+from flask import current_app, g
+
+
+def get_db():
+    """Connect to the database."""
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types = sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def close_db(e=None):
+    """Close the database connection."""
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    """Initialize the database."""
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Database initializated.')
+
+def init_app(app):
+    """Register functions with the app."""
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
